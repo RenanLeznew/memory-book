6e3fa1a71ff8a064d0b0f5764791c46548e2ec53
RenanLeznew <renanw.science@gmail.com>
2022-10-09 03:48:40 -0300

    text changes

--- src/Cover.tsx
+++ src/Cover.tsx
@@ -9,10 +9,14 @@ function Cover(){
                     <h1 className='text-8xl ml-16 text-transparent bg-clip-text bg-gradient-to-b from-red-900 via-yellow-400 to-lime-600'>NOSSO</h1> 
                     <br>
                     </br>
+                    <br>
+                    </br>
                     <h1 className='text-6xl ml-36 text-transparent bg-clip-text bg-gradient-to-b from-lime-700 via-blue-600 to-purple-900'>DAS</h1>
                 </div>
                 <div className='absolute  font-mono font-bold ml-20 right-0 mt-36 p-32'>
                     <h1 className='text-8xl ml-12 text-transparent bg-clip-text bg-gradient-to-b from-red-900 via-yellow-400 to-lime-600'>LIVRO</h1> 
+                    <br>
+                    </br>
                     <br>
                     </br>
                     <h1 className='text-6xl text-transparent bg-clip-text bg-gradient-to-b from-lime-700 via-blue-600 to-purple-900'>AVENTURAS</h1>
@@ -20,10 +24,10 @@ function Cover(){
                 <h2 className='text-8xl absolute left-64 bottom-3'>
                     🌎
                 </h2>
-                <h2 className='text-3xl absolute text-slate-800 font-bold right-64 bottom-0'>
+                <h2 className='text-6xl absolute text-slate-800 font-bold right-80 bottom-0'>
                     Renan 💜
                 </h2>
-                <h2 className='text-3xl absolute text-purple-900 font-bold right-44 bottom-0'>  Gabs</h2>
+                <h2 className='text-6xl absolute text-purple-900 font-bold right-40 bottom-0'>  Gabs</h2>
             <Link to={'/book'}>
                 <button 
                     className="w-0 h-0 absolute bottom-0 right-0 shadow-inner border-[#572307]  border-b-[85px] border-solid border-l-[85px] border-l-transparent" 
