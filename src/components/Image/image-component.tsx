bbecd8ef00bf317fccac9eef9f94391fc349b876
RenanLeznew <renanw.science@gmail.com>
2022-10-13 14:20:31 -0300

    adaptation to mobile

--- src/components/Image/image-component.tsx
+++ src/components/Image/image-component.tsx
@@ -4,7 +4,7 @@ import HoveredMessage from "../Message/message-component";
 
 export default function OurImage(props: ImageProperties ) {
   const { source, topbot, leftright, skew } = props;
-  const image_position: string = "absolute w-96 h-72 " + topbot + " " + leftright + " shadow-2xl ring-offset-2 ring-2 ring-yellow-500 " + skew;
+  const image_position: string = "absolute w-44 h-28 lg:w-96 lg:h-72 " + topbot + " " + leftright + " shadow-2xl ring-offset-2 ring-2 ring-yellow-500 " + skew;
   return (
     <img src={source} className={image_position}/>
   )
