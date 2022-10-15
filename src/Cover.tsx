c0ff81e58619082a722f9fa184b304de17f7c623
RenanLeznew <renanw.science@gmail.com>
2022-10-15 18:54:41 -0300

    landscape & portrait

--- src/Cover.tsx
+++ src/Cover.tsx
@@ -5,22 +5,43 @@ function Cover(){
         <div className="bg-gradient-to-r from-orange-900 via-yellow-800 to-orange-900 brightness-50 w-screen h-screen border-8 border-[#321414]">
             <div className="absolute left-1/2 -ml-3 h-full w-8 bg-gradient-to-r shadow-inner border-2 border-t-0 border-b-0 border-orange-900 from-orange-800 via-yellow-700 to-orange-800">
             </div>
-                <div className='absolute font-mono font-bold mr-36 lg:ml-20 mt-[50%] lg:mt-36 p-12 lg:p-32'>
-                    <h1 className='text-2xl lg:text-8xl ml-4 lg:ml-16 text-transparent bg-clip-text bg-gradient-to-b from-red-900 via-yellow-400 to-lime-600'>NOSSO</h1> 
-                    <br>
-                    </br>
-                    <br>
-                    </br>
-                    <h1 className='text-xl lg:text-6xl ml-9 lg:ml-36 text-transparent bg-clip-text bg-gradient-to-b from-lime-700 via-blue-600 to-purple-900'>DAS</h1>
+                <div className='portrait:hidden'>
+                    <div className='absolute font-mono font-bold mr-36 lg:ml-20 mt-[20%] lg:mt-36 p-12 lg:p-32'>
+                        <h1 className='text-2xl lg:text-8xl ml-4 lg:ml-16 text-transparent bg-clip-text bg-gradient-to-b from-red-900 via-yellow-400 to-lime-600'>NOSSO</h1> 
+                        <br>
+                        </br>
+                        <br>
+                        </br>
+                        <h1 className='text-xl lg:text-6xl ml-9 lg:ml-36 text-transparent bg-clip-text bg-gradient-to-b from-lime-700 via-blue-600 to-purple-900'>DAS</h1>
+                    </div>
+                    <div className='absolute mt-[28%] font-mono font-bold ml-44 lg:ml-20 lg:right-20 lg:mt-36 lg:p-32'>
+                        <h1 className='text-2xl lg:text-8xl ml-20 lg:ml-5 text-transparent bg-clip-text bg-gradient-to-b from-red-900 via-yellow-400 to-lime-600'>LIVRO</h1> 
+                        <br>
+                        </br>
+                        <br>
+                        </br>
+                        <h1 className='text-xl lg:text-6xl ml-16 text-transparent bg-clip-text bg-gradient-to-b from-lime-700 via-blue-600 to-purple-900'>AVENTURAS</h1>
+                    </div>
                 </div>
-                <div className='absolute mt-[64%] font-mono font-bold ml-44 lg:ml-20 lg:right-20 lg:mt-36 lg:p-32'>
-                    <h1 className='text-2xl lg:text-8xl ml-20 lg:ml-5 text-transparent bg-clip-text bg-gradient-to-b from-red-900 via-yellow-400 to-lime-600'>LIVRO</h1> 
-                    <br>
-                    </br>
-                    <br>
-                    </br>
+                <div className='landscape:hidden'>
+                    <div className='absolute font-mono font-bold mr-36 lg:ml-20 mt-[50%] lg:mt-36 p-12 lg:p-32'>
+                        <h1 className='text-2xl lg:text-8xl ml-4 lg:ml-16 text-transparent bg-clip-text bg-gradient-to-b from-red-900 via-yellow-400 to-lime-600'>NOSSO</h1> 
+                        <br>
+                        </br>
+                        <br>
+                        </br>
+                        <h1 className='text-xl lg:text-6xl ml-9 lg:ml-36 text-transparent bg-clip-text bg-gradient-to-b from-lime-700 via-blue-600 to-purple-900'>DAS</h1>
+                    </div>
+                    <div className='absolute mt-[64%] font-mono font-bold ml-44 lg:ml-20 lg:right-20 lg:mt-36 lg:p-32'>
+                        <h1 className='text-2xl lg:text-8xl ml-20 lg:ml-5 text-transparent bg-clip-text bg-gradient-to-b from-red-900 via-yellow-400 to-lime-600'>LIVRO</h1> 
+                        <br>
+                        </br>
+                        <br>
+                        </br>
                     <h1 className='text-xl lg:text-6xl ml-16 text-transparent bg-clip-text bg-gradient-to-b from-lime-700 via-blue-600 to-purple-900'>AVENTURAS</h1>
                 </div>
+                </div>
+                
                 <h2 className='text-xl lg:text-8xl absolute left-20 lg:left-64 bottom-3'>
                     🌎
                 </h2>
